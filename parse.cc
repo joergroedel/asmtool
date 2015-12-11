38a6c6ef267d3fb271c1f16c974b1ce6f97dad7d
Joerg Roedel <jroedel@suse.de>
2015-12-11 15:58:23 +0100

    Rewrite function compare code
    Not using the operators has the advantage that we can pass
    additional information down the call-path.
    
    Signed-off-by: Joerg Roedel <jroedel@suse.de>

--- parse.cc
+++ parse.cc
@@ -45,6 +45,104 @@ asm_file *load_file(const char *name)
 	return file;
 }
 
+static bool compare_param(asm_file *file1, asm_param& param1,
+			  asm_file *file2, asm_param &param2)
+{
+	size_t size1, size2;
+	bool ret = true;
+
+	size1 = param1.tokens.size();
+	size2 = param2.tokens.size();
+
+	if (size1 != size2)
+		return false;
+
+	for (size_t i = 0; i < size1; i++) {
+		asm_token &t1 = param1.tokens[i];
+		asm_token &t2 = param2.tokens[i];
+
+		if (t1.type != t2.type) {
+			ret = false;
+			continue;
+		}
+
+		if (t1.token == t2.token)
+			continue;
+
+		ret = false;
+	}
+
+	return ret;
+}
+
+static bool compare_instructions(asm_file *file1, asm_instruction *ins1,
+				 asm_file *file2, asm_instruction *ins2)
+{
+	size_t size1, size2;
+	int ret = true;
+
+	if (ins1->instruction != ins2->instruction)
+		return false;
+
+	size1 = ins1->params.size();
+	size2 = ins2->params.size();
+
+	if (size1 != size2)
+		return false;
+
+	/* Compare parameters */
+	for (size_t i = 0; i < size1; ++i) {
+		if (!compare_param(file1, ins1->params[i],
+				   file2, ins2->params[i]))
+			ret = false;
+	}
+
+	return ret;
+}
+
+bool compare_functions(asm_file *file1, asm_function *f1,
+		       asm_file *file2, asm_function *f2)
+{
+	size_t size1 = f1->statements.size();
+	size_t size2 = f2->statements.size();
+	bool ret = true;
+
+	/* First check if the functions have the same size */
+	if (size1 != size2)
+		return false;
+
+	/* Now iterate over the function statements */
+	for (size_t i = 0; i < size1; ++i) {
+		asm_statement &s1 = f1->statements[i];
+		asm_statement &s2 = f2->statements[i];
+
+		if (s1.type != s2.type) {
+			ret = false;
+			continue;
+		}
+
+		/*
+		 * First check for labels. They need to be identical because
+		 * they are normalized between the two versions of the function
+		 */
+		if ((s1.type == LABEL) &&
+		    (s1.obj_label->label != s2.obj_label->label)) {
+			ret = false;
+			continue;
+		}
+
+		if (s1.type == INSTRUCTION) {
+			if (!compare_instructions(file1, s1.obj_instruction,
+						  file2, s2.obj_instruction)) {
+				ret = false;
+				continue;
+			}
+		}
+	}
+
+	return ret;
+}
+
 void changes(asm_file *file1, asm_file *file2)
 {
 	for (vector<string>::const_iterator it = file2->functions.begin();
@@ -58,7 +156,7 @@ void changes(asm_file *file1, asm_file *file2)
 		asm_function *func1 = file1->get_function(*it);
 		asm_function *func2 = file2->get_function(*it);
 
-		if (!(*func1 == *func2))
+		if (!compare_functions(file1, func1, file2, func2))
 			cout << "Changed function: " << *it << endl;
 
 		delete func1;
