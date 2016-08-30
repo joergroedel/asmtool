7df00f5a13a81035a969e1ea4e8d70558bdbafc0
Joerg Roedel <jroedel@suse.de>
2016-08-30 17:08:16 +0200

    asmtool: Move symbol-info code into a seperate function
    To have a reusable function to print a information about a
    single symbol.
    
    Signed-off-by: Joerg Roedel <jroedel@suse.de>

--- info.cc
+++ info.cc
@@ -6,85 +6,92 @@
 #include "assembly.h"
 #include "info.h"
 
+static void print_one_symbol(assembly::asm_file &file,
+			     std::string &sym, assembly::asm_symbol &info,
+			     bool verbose)
+{
+	std::string scope;
+	std::string type;
+
+	switch (info.m_scope) {
+	case assembly::symbol_scope::LOCAL:
+		scope = "Local";
+		break;
+	case assembly::symbol_scope::GLOBAL:
+		scope = "Global";
+		break;
+	default:
+		scope = "Unknown";
+		break;
+	}
+
+	switch (info.m_type) {
+	case assembly::symbol_type::FUNCTION:
+		type = "Function:";
+		break;
+	case assembly::symbol_type::OBJECT:
+		type = "Object:";
+		break;
+	default:
+		type = "Unknown:";
+		break;
+	}
+
+	std::cout << std::left;
+	std::cout << std::setw(10) << type << std::setw(48) << sym;
+	std::cout << " Scope: " << std::setw(10) << scope << std::endl;
+
+	if (!verbose)
+		return;
+
+	std::unique_ptr<assembly::asm_object> obj(nullptr);
+
+	if (info.m_type == assembly::symbol_type::FUNCTION)
+		obj = file.get_function(sym, assembly::func_flags::STRIP_DEBUG);
+	else
+		obj = file.get_object(sym, assembly::func_flags::STRIP_DEBUG);
+
+	if (obj == nullptr)
+		return;
+
+	auto symbols = obj->get_symbols();
+
+	if (symbols.size() == 0)
+		return;
+
+	std::cout << "    Referenced symbols " << std::endl;
+	std::cout << "    (F - Function, O - Object, G - Global, L - Local, E - External):" << std::endl;
+
+	for (auto &s : symbols) {
+		char type = '-', scope = 'E';
+
+		if (file.has_symbol(s)) {
+			auto sym = file.get_symbol(s);
+
+			if (sym.m_type == assembly::symbol_type::FUNCTION)
+				type = 'F';
+			else if (sym.m_type == assembly::symbol_type::OBJECT)
+				type = 'O';
+
+			if (sym.m_scope == assembly::symbol_scope::GLOBAL)
+				scope = 'G';
+			else if (sym.m_scope == assembly::symbol_scope::LOCAL)
+				scope = 'L';
+		}
+
+		std::cout << "        " << s << " (" << type << scope << ')' << std::endl;
+	}
+}
+
 static void print_symbols(assembly::asm_file &file, struct info_options &opts,
 			  std::function<bool(assembly::asm_symbol&)> filter)
 {
 	file.for_each_symbol([&file, &opts, &filter](std::string sym,
 						     assembly::asm_symbol info) {
-		std::string scope;
-		std::string type;
-
 		if (!filter(info))
 			return;
 
-		switch (info.m_scope) {
-		case assembly::symbol_scope::LOCAL:
-			scope = "Local";
-			break;
-		case assembly::symbol_scope::GLOBAL:
-			scope = "Global";
-			break;
-		default:
-			scope = "Unknown";
-			break;
-		}
-
-		switch (info.m_type) {
-		case assembly::symbol_type::FUNCTION:
-			type = "Function:";
-			break;
-		case assembly::symbol_type::OBJECT:
-			type = "Object:";
-			break;
-		default:
-			type = "Unknown:";
-			break;
-		}
-
-		std::cout << std::left;
-		std::cout << std::setw(10) << type << std::setw(48) << sym;
-		std::cout << " Scope: " << std::setw(10) << scope << std::endl;
-
-		if (!opts.verbose)
-			return;
-
-		std::unique_ptr<assembly::asm_object> obj(nullptr);
-
-		if (info.m_type == assembly::symbol_type::FUNCTION)
-			obj = file.get_function(sym, assembly::func_flags::STRIP_DEBUG);
-		else
-			obj = file.get_object(sym, assembly::func_flags::STRIP_DEBUG);
-
-		if (obj == nullptr)
-			return;
-
-		auto symbols = obj->get_symbols();
-
-		if (symbols.size() == 0)
-			return;
-
-		std::cout << "    Referenced symbols " << std::endl;
-		std::cout << "    (F - Function, O - Object, G - Global, L - Local, E - External):" << std::endl;
-
-		for (auto &s : symbols) {
-			char type = '-', scope = 'E';
-
-			if (file.has_symbol(s)) {
-				auto sym = file.get_symbol(s);
-
-				if (sym.m_type == assembly::symbol_type::FUNCTION)
-					type = 'F';
-				else if (sym.m_type == assembly::symbol_type::OBJECT)
-					type = 'O';
-
-				if (sym.m_scope == assembly::symbol_scope::GLOBAL)
-					scope = 'G';
-				else if (sym.m_scope == assembly::symbol_scope::LOCAL)
-					scope = 'L';
-			}
-
-			std::cout << "        " << s << " (" << type << scope << ')' << std::endl;
-		}
+		print_one_symbol(file, sym, info, opts.verbose);
 	});
 }
 
